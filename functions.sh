cf0fa3cbe7d6d4dcc65b6cc79b12413be456edc9
Malek Musleh <mmusleh@isi.edu>
2013-06-05 15:06:27 -0400

    Added missing helper script functions

---
+++ functions.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+# function.sh
+# Malek Musleh                                                                                                       
+# mmusleh@isi.edu                                                                                                    
+# May. 15, 2013                                                                                                      
+#                                                                                                                    
+# (c) 2013 USC/ISI                                                                                                   
+#                                                                                                                    
+# This script is provided for a reference only.                                                                      
+# Its functional correctness is not guaranteed.                                                                      
+# It contains helper functions related to volume usage in Openstack    
+
+# Checks an environment variable is not set or has length 0 OR if the                            
+# exit code is non-zero and prints "message" and exits                                                              
+# NOTE: env-var is the variable name without a '$'                                                                   
+# die_if_not_set $LINENO env-var "message"                                                                        
+
+function die_if_not_set() {
+    local exitcode=$?
+    FXTRACE=$(set +o | grep xtrace)
+    set +o xtrace
+    local line=$1; shift
+    local evar=$1; shift
+    if ! is_set $evar || [ $exitcode != 0 ]; then
+        die $line "$*"
+    fi
+    $FXTRACE
+}
+
+# Prints line number and "message" in error format                                                               
+# err $LINENO "message"                                                                                          
+function err() {
+    local exitcode=$?
+    errXTRACE=$(set +o | grep xtrace)
+    set +o xtrace
+    local msg="[ERROR] $0:$1 $2"
+    echo $msg 1>&2;
+    if [[ -n ${SCREEN_LOGDIR} ]]; then
+        echo $msg >> "${SCREEN_LOGDIR}/error.log"
+    fi
+    $errXTRACE
+    return $exitcode
+}
+
+# Create the log file (if necessary) and append a timestamp                                                       
+function start_log() {
+    # delete the log file if it exists from previous installation                                                
+    local LOG_FILE=$1
+
+    echo "starting log file: ${LOG_FILE}"
+
+    rm -rf ${LOG_FILE}
+    set -o xtrace
+    touch ${LOG_FILE}
+    chmod 600 ${LOG_FILE}
+    set +o xtrace
+    echo "$(date) $BASENAME: installation process initiated ($PARAMETERS)" >> $LOG_FILE
+
+}
+
+# Append msg to log file
+function write_log() {
+    local message="$1"
+    local LOG_FILE="$2"
+    sed -i'' -e '/^+ set +o xtrace$/d' $LOG_FILE
+    echo "$(date) $message" >> $LOG_FILE
+}
+
+# Remove clutter from log and append a timestamp                                                              
+function finalize_log() {
+    local message="$1"
+    local LOG_FILE="$2"
+    sed -i'' -e '/^+ set +o xtrace$/d' $LOG_FILE
+    echo "$(date) $BASENAME: $message ($PARAMETERS)" >> $LOG_FILE
+}
+
+# Function to pretty print step# and description of test
+function print_test_msg() {
+    local stepNum="$1"
+    local message="$2"
+    echo " "
+    echo "---------------------------------------------------------------------------"
+    echo " ${stepNum}: ${message}"
+    echo "---------------------------------------------------------------------------"
+    echo " "
+}
+
+# Function to pretty print step#, message, and command used
+function print_test_command_msg() {
+    local stepNum="$1"
+    local message="$2"
+    local command="$3"
+    echo " "
+    echo "---------------------------------------------------------------------------"
+    echo " ${stepNum}: ${message}"
+    echo " command: ${command}"
+    echo "---------------------------------------------------------------------------"
+    echo " "
+}
+
+# Function to ping host instance/machine
+function ping_host(){
+        ping -q -c 1 $1 > /dev/null 2>&1
+        local myIP=$?
+        echo $myIP
+}
+
+function remove_known_hosts() {
+
+    local user=`whoami`
+
+    if [ "${user}" == "root" ]
+    then
+	rm -rf /root/.ssh/known_hosts
+    else
+	RET=`rm -rf /home/${user}/.ssh/known_hosts`
+    fi
+
+}
+
+# Grab a numbered field from python prettytable output             
+# Fields are numbered starting with 1                                                                          
+# Reverse syntax is supported: -1 is the last field, -2 is second to last, etc.                             
+# get_field field-number                                                                                     
+function get_field() {
+    while read data; do
+    if [ "$1" -lt 0 ]; then
+            field="(\$(NF$1))"
+	    else
+            field="\$$(($1 + 1))"
+	    fi
+        echo "$data" | awk -F'[ \t]*\\|[ \t]*' "{print $field}"
+    done
+}
+
+
+# Wait at most $1 seconds for cmd $2 to return a non-empty string.
+# Returns status 0 command succeeded
+function timeout_check() {
+    seconds=$1
+    cmd=$2
+    # echo timeout_check $seconds $cmd
+    ( cmdpid=$BASHPID;
+	( sleep $seconds; kill $cmdpid &> /dev/null) \
+	& while [ -z "`eval $cmd`" ]; do
+	    sleep 5
+	done 
+    )
+}
+
+
+
+function get_status(){
+
+        local mySTATUS=`nova show $1 | grep "status" | awk '{print $4}'`
+        echo $mySTATUS
+}
+
+# TODO: The following 2 functions do the same, and should be merged
+function makeAddKey {
+  # $1 is keypair name, $2 is key file name to be created, $3 is nova params
+  ssh-keygen -f $2 -N ''
+  chmod 600 $2.pub
+  nova $3 keypair-add --pub-key "$2.pub" $1
+}
+
+function do_create_keypair(){
+
+        source $1
+        if [ -e "$2" ];
+        then
+                return 0
+        else
+                ssh-keygen -N "" -f $2
+                nova keypair-add --pub_key $2.pub $2
+        fi
+        return 0
+
+}
+
+function do_delete_keypair(){
+        source $1
+        nova keypair-delete $2
+        rm -rf $2 $2.pub
+        return 0
+}
+
+function sendSshAndGet {
+  # $1 - result to be returned
+  local keyfile=$2
+  local ip=$3
+  local cmd=$4
+  local res=`ssh -i $keyfile -o StrictHostKeyChecking=no root@$ip $cmd`
+  eval "$1='$res'"
+}
+
---
+++ volume.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+# volume.sh
+# Malek Musleh                                                                                                 
+# mmusleh@isi.edu                                                                                              
+# May. 15, 2013                                                                                               
+#                                                                                                           
+# (c) 2013 USC/ISI                                                                                        
+#                                                                                                             
+# This script is provided for a reference only.                                                                    
+# Its functional correctness is not guaranteed.                                                                      
+# It contains helper functions related to volume usage in Openstack
+
+# Function to get the volume id
+function get_volume_id() {
+
+    VOL_NAME=$1
+    VOL_ID=$(cinder list | grep $VOL_NAME | head -1 | get_field 1)
+    die_if_not_set $LINENO VOL_ID "Failure retrieving volume ID for $VOL_NAME"
+    echo $VOL_ID
+}
+
+# Function to check if a volume exists
+function volume_exists() {
+
+    VOL_NAME=$1
+    if [[ -n $(cinder list | grep $VOL_NAME | head -1 | get_field 2) ]]; then
+	echo $LINENO "true"
+    else
+	echo $LINENO "false"
+    fi
+}
+
+# Function to check the status of a volume
+function volume_status() {
+    VOL_NAME=$1
+    RET=`euca-describe-volumes ${VOL_NAME}`
+    status=`echo $RET | awk '{ print $5}'`
+    echo ${status}
+}
+
+# Function to check the state of a volume
+function volume_state() {
+    VOL_NAME=$1
+    RET=`euca-describe-volumes ${VOL_NAME}`
+    status=`echo $RET | awk '{ print $11}'`
+    volume=`echo $RET | awk '{ print $2}'`
+    user=`echo $RET | awk '{ print $4}'`
+    state=`echo $RET | awk '{ print $5}'`
+    displayName=`echo $RET | awk '{ print $3}'`
+    echo ${status}
+}
+
+function volume_attached() {
+    VOL_NAME=$1
+    status=$(volume_status "${VOL_NAME}")
+    state=$(volume_state "${VOL_NAME}")
+
+    if [ "${state}" != "attached" ] && [ "${status}" != "in-use" ]
+    then
+	echo "false"
+    else
+	echo "true"
+    fi
+}
+
+function volume_available() {
+    VOL_NAME=$1
+    status=$(volume_status "${VOL_NAME}")
+    state=$(volume_state "${VOL_NAME}")
+    if [ "${state}" != "available" ] || [ "${status}" != "available" ]
+    then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
+
+function detach_volume() {
+    VOL_NAME=$1
+    RET=`euca-detach-volume ${VOL_NAME}`
+    # give some time to detach
+    sleep 30
+}
+
+
+function delete_volume() {
+
+    VOL_NAME=$1
+    RET=`euca-detach-volume ${VOL_NAME}`
+    # give some time to delete
+    sleep 30
+}
+
+# Wrapper to cycle through and call delete_volume
+function delete_all_volumes() {
+
+    echo "delete_all_volumes"
+
+}
+
+
+
+##### NOVA VOLUME RELATED FUNCTIONS ######
+
+# Create a volume and wait until it's either error or evailable                                   
+function nova_createVolume {
+  # will return status as the first parameter                                                                   
+  # User credentials for calling nova                                                                           
+  local params=$2
+  # Volume name                                                                                                
+  local vol_name=$3
+  # Volume size                                                                                               
+  local vol_size=$4
+
+  nova $params volume-create --display_name $vol_name $vol_size
+  sleep 4
+  local check="nova $params volume-list | grep $vol_name | awk '{ print \$4 }'"
+  local status=`eval $check`
+  while [ "$status" == "creating" ]; do
+    echo "Volume is not ready yet: $status"
+    sleep 4
+    let status=`eval $check`
+  done
+  eval "$1=$status"
+}
+
+
+# create an instance and wait until it it's ready. If it fails don't return anything to the caller, just exit
+function nova_bootInstance {
+  local params=$1
+  local name=$2
+  local flavor=$3
+  local image=$4
+  local other=$5
+
+  echo "Trying to boot instance $name"
+  nova $params boot --flavor $flavor --image $image $other $name
+  local check="nova $params list | grep $name | awk '{ print \$6 }'"
+  sleep 2
+  local status=`eval $check`
+  while [ "$status" == "BUILD" ]; do
+    echo "Building instance: $status"
+    sleep 5
+    status=`eval $check`
+  done
+  if [ "$status" != "ACTIVE" ];
+  then
+     echo "Failed to start instance $name $flavor $image: $status"
+     exit -100
+  fi
+}
+
+# Attach volume.                                                                                                
+function nova_attachVolume {
+  local params=$1
+  local instance=$2
+  local volume_name=$3
+  local device=$4
+
+  echo "Trying to attach volume $volume_name to $instance"
+
+  local volume_id=""
+  getVolumeField volume_id $volume_name "$params" 2
+
+  nova $params volume-attach $instance $volume_id $device
+  sleep 30
+}
+
+function nova_detachVolume {
+  local params=$1
+  local instance=$2
+  local volume_id=""
+  echo "Trying to detach volume $3 from $instance"
+
+  nova_getVolumeField volume_id $3 "$params" 2
+  nova $params volume-detach $instance $volume_id
+  sleep 5
+}
+
+# Get attachement instance of volume $2. Return as 1  
+function nova_attachedTo {
+  getVolumeField $1 $2 "$3" 12
+}
+
+function nova_getVolumeField {
+  local vol_name=$2
+  local params=$3
+  local index=$4
+  local check=`nova $params volume-list | grep $2 | awk -v a="$index" '{ print \$a }'`
+  eval "$1=$check"
+}
+
+function nova_volumeStatus {
+  getVolumeField $1 $2 "$3" 4
+}
+
